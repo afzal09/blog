61eee1fa91844a95eda80056bb2210cd14a1f1c5
Afzal <afzalmomin2003@gmail.com>
2024-01-14 12:19:26 +0530

    posts table created

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
@@ -8,6 +9,17 @@ class User(db.Model):
     username: orm.Mapped[str] = orm.mapped_column(sa.String(64), index=True, unique=True)
     email: orm.Mapped[str] = orm.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: orm.Mapped[Optional[str]] = orm.mapped_column(sa.String(256))
+    posts: orm.WriteOnlyMapped['Posts'] = orm.relationship(back_populates="author")
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+
+class Post(db.Model):
+    id: orm.Mapped[int] = orm.mapped_column(primary_key = True)
+    body:orm.Mapped[str] = orm.mapped_column(sa.String(140))
+    timestamp:orm.Mapped[datetime] = orm.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    user_id:orm.Mapped[int] = orm.mapped_column(sa.ForeignKey(User.id),index=True)
+    author:orm.Mapped[User] = orm.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
